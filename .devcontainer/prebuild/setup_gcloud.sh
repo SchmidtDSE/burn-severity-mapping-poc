ebabff7eb6066cfb9153a8c1f5cf3148a31fe5e2
nick gondek <GondekNP@gmail.com>
2023-12-13 15:10:14 -0800

    fix gcp install paths

--- .devcontainer/prebuild/setup_gcloud.sh
+++ .devcontainer/prebuild/setup_gcloud.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-457.0.0-linux-x86_64.tar.gz -o gcloud.tar.gz
+curl -o "gcloud.tar.gz" "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-457.0.0-linux-x86_64.tar.gz"
 tar -xzf gcloud.tar.gz 
-./google-cloud-sdk/install.sh 
+./google-cloud-sdk/install.sh --quiet
 rm -rf gcloud.tar.gz google-cloud-sdk
-./google-cloud-sdk/bin/gcloud init
