62f1ed9e090d1ccb84a8c324664f8e435ad43103
nick gondek <GondekNP@gmail.com>
2023-12-23 18:40:37 +0000

    Okay - drop all this rusting since these don't really need to be in the same container anyways. Set up as a docker compose grabbing the docker image from ghcr.io, then we can try it out as intended and potentially just let that service be, sending it our http cog urls as a secondary service. Would be nice to not have the overhead of all the other services but oh well, not bad for now if it works.

--- app.py
+++ app.py
@@ -6,13 +6,17 @@ from src.util.sftp import SFTPClient
 from src.util.aws_secrets import get_ssh_secret, get_signed_s3_url
 from titiler.core import factory
 from titiler.core.errors import DEFAULT_STATUS_CODES, add_exception_handlers
+import uvicorn
 
 from pydantic import BaseModel
 
 # app = Flask(__name__)
 app = FastAPI()
 cog = factory.TilerFactory()
-app.include_router(cog.router, prefix="/cog", tags=["Cloud Optimized GeoTIFF"])
+# app.include_router(cog.router, prefix="/cog", tags=["Cloud Optimized GeoTIFF"])
+# add_exception_handlers(app, DEFAULT_STATUS_CODES)
+
+app.include_router(cog.router)
 add_exception_handlers(app, DEFAULT_STATUS_CODES)
 
 # create an SFTP client instance
@@ -67,19 +71,23 @@ def analyze_burn(body: AnaylzeBurnPOSTBody):
         return f"Error: {e}", 400
 
 
-@app.get("/{fire_event_name}/tiles/{z}/{x}/{y}")
-def read_polygon(fire_event_name: str, z: int, x: int, y: int):
-    try:
-        # Fetch the COG file URL corresponding to the given polygon
-        cog_s3_url = sftp_client.available_cogs[fire_event_name]
-        cog_signed_url = get_signed_s3_url(cog_s3_url, bucket_name=S3_BUCKET_NAME)
+# @app.get("/{fire_event_name}/tiles/{z}/{x}/{y}")
+# def read_polygon(fire_event_name: str, z: int, x: int, y: int):
+#     try:
+#         # Fetch the COG file URL corresponding to the given polygon
+#         cog_s3_url = sftp_client.available_cogs[fire_event_name]
+#         # cog_signed_url = get_signed_s3_url(cog_s3_url, bucket_name=S3_BUCKET_NAME)
+#         cog_https_url = (
+#             f"https://burn-severity.s3.us-east-2.amazonaws.com/public/{cog_s3_url}"
+#         )
 
-    except KeyError:
-        raise HTTPException(status_code=404, detail="Fire event not found")
+#     except KeyError:
+#         raise HTTPException(status_code=404, detail="Fire event not found")
 
-    # Pass all request parameters along to the COG Tiler
-    return cog.tile(cog_signed_url, z, x, y)
+#     # Pass all request parameters along to the COG Tiler
+#     return cog.tile(cog_https_url, z, x, y)
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5050)
+    # app.run(debug=True, port=5050)
+    uvicorn.run(app=app, host="127.0.0.1", port=5050, log_level="debug")
