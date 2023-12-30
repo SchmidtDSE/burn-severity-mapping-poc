9eb601ae4f9658850bfbb7653126fc229a1870d6
nick gondek <GondekNP@gmail.com>
2023-12-30 23:05:18 +0000

    try to solve this google cloud sdk being in the wrong spot after prebuild...

--- .devcontainer/prebuild/setup_gcloud.sh
+++ .devcontainer/prebuild/setup_gcloud.sh
@@ -2,6 +2,7 @@
 
 curl -o "gcloud.tar.gz" "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-457.0.0-linux-x86_64.tar.gz"
 tar -xzf gcloud.tar.gz 
-./google-cloud-sdk/install.sh --quiet --path-prefix=$HOME
+mv google-cloud-sdk $HOME/
+$HOME/google-cloud-sdk/install.sh --quiet
 rm -rf gcloud.tar.gz
 export PATH=$PATH:$HOME/google-cloud-sdk/bin
