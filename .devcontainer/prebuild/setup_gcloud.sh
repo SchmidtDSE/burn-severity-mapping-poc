bb2b15c7c27394201c46f7e8dad687603333a437
nick gondek <GondekNP@gmail.com>
2023-12-30 23:30:48 +0000

    one more path try... wtf

--- .devcontainer/prebuild/setup_gcloud.sh
+++ .devcontainer/prebuild/setup_gcloud.sh
@@ -2,7 +2,7 @@
 
 curl -o "gcloud.tar.gz" "https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-457.0.0-linux-x86_64.tar.gz"
 tar -xzf gcloud.tar.gz 
-mv google-cloud-sdk /root
-$HOME/google-cloud-sdk/install.sh --quiet
+mv google-cloud-sdk /usr/local
+/usr/local/google-cloud-sdk/install.sh --quiet
 rm -rf gcloud.tar.gz
-export PATH=$PATH:/root/google-cloud-sdk/bin
+export PATH=$PATH:/usr/local/google-cloud-sdk/bin
