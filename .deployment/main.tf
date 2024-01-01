1b198030d1c776f6e79c9c6a39df7debf41aeb26
nick gondek <GondekNP@gmail.com>
2023-12-31 18:41:42 -0800

    OIDC AWS side

--- .deployment/main.tf
+++ .deployment/main.tf
@@ -25,7 +25,7 @@ provider "google" {
 # Get google project info
 data "google_project" "project" {}
 
-### AWS Transfer - SFTP Server
+### AWS ###
 
 # First the server itself
 resource "aws_transfer_server" "tf-sftp-burn-severity" {
@@ -47,6 +47,35 @@ resource "aws_transfer_server" "tf-sftp-burn-severity" {
 #   body      = file("<PUBLIC_KEY_FILE_PATH>")
 # }
 
+# Set up STS to allow the GCP server to assume a role for AWS secrets
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    principals {
+      type        = "Federated"
+      identifiers = ["arn:aws:iam::557418946771:oidc-provider/https://${data.google_project.project.number}"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "https://${data.google_project.project.number}.svc.id.goog:sub"
+
+      values = [
+        "system:serviceaccount:${data.google_project.project.number}.svc.id.goog[default/${google_service_account.access_aws_secrets.account_id}]"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name               = "aws_secrets_access_role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+
+### GCP ###
 
 # Create a Cloud Run service
 resource "google_cloud_run_service" "tf-rest-burn-severity" {
@@ -55,6 +84,7 @@ resource "google_cloud_run_service" "tf-rest-burn-severity" {
 
   template {
     spec {
+      service_account_name = google_service_account.access_aws_secrets.email
       containers {
         image = "us-docker.pkg.dev/cloudrun/container/placeholder"
       }
@@ -92,41 +122,48 @@ resource "google_iam_workload_identity_pool_provider" "oidc" {
 }
 
 # Create the IAM service account for GitHub Actions
-resource "google_service_account" "default" {
+resource "google_service_account" "github_actions" {
   account_id  = "github-actions-service-account"
   display_name = "Github Actions Service Account"
   description = "This service account is used by GitHub Actions"
   project     = "dse-nps"
 }
 
+# Create the IAM service account for the Cloud Run service to access AWS
+resource "google_service_account" "access_aws_secrets" {
+  account_id   = "cloud-run-aws-access"
+  display_name = "Cloud Run AWS Access Service Account"
+  description  = "This service account is used by the Cloud Run service to access AWS Secrets"
+  project      = "dse-nps"
+}
 
 # Give the service account permissions to deploy to Cloud Run, and to Cloud Build, and to the Workload Identity Pool
 resource "google_project_iam_member" "run_admin" {
   project  = "dse-nps"
   role     = "roles/run.admin" 
-  member   = "serviceAccount:${google_service_account.default.email}"
+  member   = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
 resource "google_project_iam_member" "cloudbuild_builder" {
   project  = "dse-nps"
   role     = "roles/cloudbuild.builds.builder"
-  member   = "serviceAccount:${google_service_account.default.email}"
+  member   = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
 resource "google_project_iam_member" "SA_get_access_token" {
   project  = "dse-nps"
   role    = "roles/iam.serviceAccountTokenCreator"
-  member   = "serviceAccount:${google_service_account.default.email}"
+  member   = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
 resource "google_project_iam_member" "run_service_agent" {
   project  = "dse-nps"
   role    = "roles/run.serviceAgent"
-  member   = "serviceAccount:${google_service_account.default.email}"
+  member   = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
 resource "google_service_account_iam_binding" "workload_identity_user" {
-  service_account_id = google_service_account.default.name
+  service_account_id = google_service_account.github_actions.name
   role               = "roles/iam.workloadIdentityUser"
   members            = [
     "principalSet://iam.googleapis.com/projects/${data.google_project.project.number}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.pool.workload_identity_pool_id}/attribute.repository/SchmidtDSE/burn-severity-mapping-poc"
