4e21ad388da579135d5104a8797493e5b2f33901
nick gondek <GondekNP@gmail.com>
2024-01-06 23:24:19 +0000

    getting some weird behavior with the masked array output from algo... weird masked blue thing, seems not not be recognizing that this is gray + alpha but going to see how leaflet interprets

--- src/lib/titiler_algorithms.py
+++ src/lib/titiler_algorithms.py
@@ -8,26 +8,32 @@ class Classify(BaseAlgorithm):
     # Parameters
     thresholds: dict # There is no default, which means calls to this algorithm without any parameter will fail
 
-    # We don't set any metadata for this Algorithm
-
     def __call__(self, img: ImageData) -> ImageData:
-        # Convert image data to entirely 100s
-        # convert thresholds keys to float
+
         float_thresholds = {float(k):v for k,v in self.thresholds.items()}
 
         checks = [img.data < float(threshold) for threshold in float_thresholds]
-
         values = list(float_thresholds.values())
-
         classified = np.select(checks, values, default=0).astype(np.uint8)
+
+        # Generate mask where classified is not 0 
+        mask = classified != 0
+
+        # Squeeze 1-length dim
+        classified_squeezed = np.squeeze(classified)
+
+        # Convert classified_squeezed to a MaskedArray
+        final_img = np.ma.MaskedArray(classified_squeezed, mask=mask)
 
         # Create output ImageData
         return ImageData(
-            classified,
+            final_img,
             assets=img.assets,
             crs=img.crs,
             bounds=img.bounds,
         )
+
+
 
 algorithms = default_algorithms.register(
     {
