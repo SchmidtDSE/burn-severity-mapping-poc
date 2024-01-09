35a561a77869adb146cb08be930f48fad8a0a18e
nick gondek <GondekNP@gmail.com>
2024-01-09 18:40:27 +0000

    color scale looks right enough

--- app.py
+++ app.py
@@ -151,7 +151,7 @@ def serve_map(request: Request, fire_event_name: str, burn_metric: str, manifest
     tileserver_endpoint = 'https://tf-rest-burn-severity-ohi6r6qs2a-uc.a.run.app'
     # tileserver_endpoint = 'http://localhost:5050'
     cog_url = f"https://burn-severity-backend.s3.us-east-2.amazonaws.com/public/{fire_event_name}/{burn_metric}.tif"
-    cog_tileserver_url_prefix = tileserver_endpoint + f"/cog/tiles/WebMercatorQuad/{{z}}/{{x}}/{{y}}.png?url={cog_url}&nodata=-99&return_mask=true&algorithm=classify&algorithm_params="
+    cog_tileserver_url_prefix = tileserver_endpoint + f"/cog/tiles/WebMercatorQuad/{{z}}/{{x}}/{{y}}.png?url={cog_url}&nodata=-99&return_mask=true"
 
     fire_metadata = manifest[fire_event_name]
     fire_metadata_json = json.dumps(fire_metadata)
