e3fc2ad7a7fcd08eadb1e6181b80de6fc9fb27d1
nick gondek <GondekNP@gmail.com>
2024-01-22 18:17:29 +0000

    dummy push - try todo with new src string

--- src/util/sftp.py
+++ src/util/sftp.py
@@ -12,7 +12,7 @@ import geopandas as gpd
 from google.cloud import logging as cloud_logging
 
 
-# TODO: Convert to agnostic Boto client 
+# TODO: Convert to agnostic Boto client
 # Use the slick smart-open library to handle S3 connections. This maintains the agnostic nature
 # of sftp, not tied to any specific cloud provider, but is way more efficient than paramiko/sftp in terms of $$
 class SFTPClient:
