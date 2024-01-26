69a78fa791944d61be2a612b75d9d622d035eb6d
nick gondek <GondekNP@gmail.com>
2024-01-26 20:59:25 +0000

    fix issues with quotes

--- .devcontainer/scripts/export_tofu_dotenv.sh
+++ .devcontainer/scripts/export_tofu_dotenv.sh
@@ -5,7 +5,7 @@ tofu refresh
 
 export gcp_cloud_run_endpoint="$(tofu output gcp_cloud_run_endpoint)"
 export s3_from_gcp_arn="$(tofu output s3_from_gcp_arn)"
-export gcp_service_account_s3_email="$(tofu output gcp_service_account_s3_email)"
+export gcp_service_account_s3_email=$(tofu output gcp_service_account_s3_email)
 
 echo "# TOFU ENV VARS" >> /workspace/.devcontainer/.env
 echo "ENV=LOCAL" >> /workspace/.devcontainer/.env
@@ -14,4 +14,5 @@ echo "GCP_CLOUD_RUN_ENDPOINT=$gcp_cloud_run_endpoint" >> /workspace/.devcontaine
 echo "GCP_SERVICE_ACCOUNT_S3_EMAIL=$gcp_service_account_s3_email" >> /workspace/.devcontainer/.env
 
 # Set gcloud config to allow local development to behave as if it were running in the cloud
-gcloud config set auth/impersonate_service_account $gcp_service_account_s3_email
+gcloud auth application-default login --impersonate-service-account $gcp_service_account_s3_email
+echo "GOOGLE_APPLICATION_CREDENTIALS=/root/.config/gcloud/application_default_credentials.json" >> /workspace/.devcontainer/.env
