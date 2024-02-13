756e2ae569d68ce2c20d9ee632a3d90dbd48a0f9
nick gondek <GondekNP@gmail.com>
2024-02-13 21:10:06 +0000

    working with proper pickling of xarrays

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ import pystac
 import rioxarray as rxr
 import numpy as np
 import xarray as xr
+import pickle
 
 ### PyStac ItemCollection
 
@@ -19,32 +20,24 @@ def test_stac_items():
 
 @pytest.fixture
 def test_valid_xarray():
-    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir.tif")
-    test_xarray_swir = rxr.open_rasterio("tests/assets/test_swir.tif")
-
-    return xr.concat(
-        [test_xarray_nir, test_xarray_swir],
-        dim="band",
-    )
+    with open("tests/assets/test_imagery_with_time_xarray.pkl", "rb") as f:
+        test_valid_xarray = pickle.load(f)
+    return test_valid_xarray
 
 
 @pytest.fixture
 def test_nan_xarray():
-    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir.tif")
-    nan_array = xr.full_like(test_xarray_nir, np.nan)
-    test_nan_xarray = xr.concat(
-        [nan_array.rename(band="B8A"), nan_array.rename(band="B12")], dim="band"
-    )
+    with open("tests/assets/test_imagery_with_time_xarray.pkl", "rb") as f:
+        test_valid_xarray = pickle.load(f)
+    test_nan_xarray = xr.full_like(test_valid_xarray, np.nan)
     return test_nan_xarray
 
 
 @pytest.fixture
 def test_zero_xarray():
-    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir.tif")
-    zero_array = xr.full_like(test_xarray_nir, 0)
-    test_zero_xarray = xr.concat(
-        [zero_array.rename(band="B8A"), zero_array.rename(band="B12")], dim="band"
-    )
+    with open("tests/assets/test_imagery_with_time_xarray.pkl", "rb") as f:
+        test_valid_xarray = pickle.load(f)
+    test_zero_xarray = xr.full_like(test_valid_xarray, 0)
     return test_zero_xarray
 
 
@@ -53,27 +46,22 @@ def test_zero_xarray():
 
 @pytest.fixture
 def test_reduced_xarray():
-    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir_reduced.tif")
-    test_xarray_swir = rxr.open_rasterio("tests/assets/test_swir_reduced.tif")
-    test_reduced_xarray = xr.concat([test_xarray_nir, test_xarray_swir], dim="band")
+    with open("tests/assets/test_imagery_time_reduced_xarray.pkl", "rb") as f:
+        test_reduced_xarray = pickle.load(f)
     return test_reduced_xarray
 
 
 @pytest.fixture
 def test_reduced_nan_xarray():
-    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir_reduced.tif")
-    nan_array = xr.full_like(test_xarray_nir, np.nan)
-    test_reduced_nan_xarray = xr.concat(
-        [nan_array.rename(band="B8A"), nan_array.rename(band="B12")], dim="band"
-    )
-    return test_reduced_nan_xarray
+    with open("tests/assets/test_imagery_time_reduced_xarray.pkl", "rb") as f:
+        test_reduced_xarray = pickle.load(f)
+    test_nan_array = xr.full_like(test_reduced_xarray, np.nan)
+    return test_nan_array
 
 
 @pytest.fixture
 def test_reduced_zero_xarray():
-    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir_reduced.tif")
-    zero_array = xr.full_like(test_xarray_nir, 0)
-    test_reduced_zero_xarray = xr.concat(
-        [zero_array.rename(band="B8A"), zero_array.rename(band="B12")], dim="band"
-    )
+    with open("tests/assets/test_imagery_time_reduced_xarray.pkl", "rb") as f:
+        test_reduced_xarray = pickle.load(f)
+    test_reduced_zero_xarray = xr.full_like(test_reduced_xarray, 0)
     return test_reduced_zero_xarray
