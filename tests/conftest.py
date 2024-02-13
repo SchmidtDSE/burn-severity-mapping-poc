d28a1066c32917c6524db79b8ec13b28f56535c6
nick gondek <GondekNP@gmail.com>
2024-02-13 20:40:48 +0000

    tests running but something fishy with the demo tiffs I made... for some reason, time seems to have been renamed or hnever existed in the first place. Will be better to just save them as multiband in the first place.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,15 @@
 import pytest
 import pystac
-import rasterio as rio
-import xarray as xr
+import rioxarray as rxr
 import numpy as np
+import xarray as xr
 
 ### PyStac ItemCollection
 
 
 @pytest.fixture
 def test_stac_items():
-    with open("assets/test_stac_items.json") as f:
+    with open("tests/assets/test_stac_items.json") as f:
         test_stac_items = pystac.read_file(f.read())
     return test_stac_items
 
@@ -19,17 +19,18 @@ def test_stac_items():
 
 @pytest.fixture
 def test_valid_xarray():
-    with open("assets/test_nir.tif") as f:
-        test_xarray_nir = rio.open(f)
-    with open("assets/test_swir.tif") as f:
-        test_xarray_swir = rio.open(f)
-    return xr.concat([test_xarray_nir, test_xarray_swir], dim="band")
+    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir.tif")
+    test_xarray_swir = rxr.open_rasterio("tests/assets/test_swir.tif")
+
+    return xr.concat(
+        [test_xarray_nir, test_xarray_swir],
+        dim="band",
+    )
 
 
 @pytest.fixture
 def test_nan_xarray():
-    with open("assets/test_nir.tif") as f:
-        test_xarray_nir = rio.open(f)
+    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir.tif")
     nan_array = xr.full_like(test_xarray_nir, np.nan)
     test_nan_xarray = xr.concat(
         [nan_array.rename(band="B8A"), nan_array.rename(band="B12")], dim="band"
@@ -39,8 +40,7 @@ def test_nan_xarray():
 
 @pytest.fixture
 def test_zero_xarray():
-    with open("assets/test_nir.tif") as f:
-        test_xarray_nir = rio.open(f)
+    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir.tif")
     zero_array = xr.full_like(test_xarray_nir, 0)
     test_zero_xarray = xr.concat(
         [zero_array.rename(band="B8A"), zero_array.rename(band="B12")], dim="band"
@@ -53,18 +53,15 @@ def test_zero_xarray():
 
 @pytest.fixture
 def test_reduced_xarray():
-    with open("assets/test_nir_reduced.tif") as f:
-        test_xarray_nir = rio.open(f)
-    with open("assets/test_swir_reduced.tif") as f:
-        test_xarray_swir = rio.open(f)
+    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir_reduced.tif")
+    test_xarray_swir = rxr.open_rasterio("tests/assets/test_swir_reduced.tif")
     test_reduced_xarray = xr.concat([test_xarray_nir, test_xarray_swir], dim="band")
     return test_reduced_xarray
 
 
 @pytest.fixture
 def test_reduced_nan_xarray():
-    with open("assets/test_nir_reduced.tif") as f:
-        test_xarray_nir = rio.open(f)
+    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir_reduced.tif")
     nan_array = xr.full_like(test_xarray_nir, np.nan)
     test_reduced_nan_xarray = xr.concat(
         [nan_array.rename(band="B8A"), nan_array.rename(band="B12")], dim="band"
@@ -74,8 +71,7 @@ def test_reduced_nan_xarray():
 
 @pytest.fixture
 def test_reduced_zero_xarray():
-    with open("assets/test_nir_reduced.tif") as f:
-        test_xarray_nir = rio.open(f)
+    test_xarray_nir = rxr.open_rasterio("tests/assets/test_nir_reduced.tif")
     zero_array = xr.full_like(test_xarray_nir, 0)
     test_reduced_zero_xarray = xr.concat(
         [zero_array.rename(band="B8A"), zero_array.rename(band="B12")], dim="band"
