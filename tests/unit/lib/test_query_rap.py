4807ed48dcbeb709c7d95700857c9eac5b0ce9e2
nick gondek <GondekNP@gmail.com>
2024-02-22 20:49:36 +0000

    working - setting the return of mock_open to  and then mocking specifically rio.isel_window. Issue was that the window was actually working fine, but the offset was inaccurate due to mocking the return from RAP (since a much tinier xarray).

--- tests/unit/lib/test_query_rap.py
+++ tests/unit/lib/test_query_rap.py
@@ -1,12 +1,14 @@
 import pytest
 from src.lib.query_rap import rap_get_biomass
-import unittest.mock as mock
+from unittest.mock import patch
+from rioxarray.raster_array import RasterArray
 import xarray as xr
 import geopandas as gpd
 from shapely.geometry import Polygon
 
 
-@mock.patch("rioxarray.open_rasterio")
+@patch("rioxarray.open_rasterio")
+# @patch.object(RasterArray, "isel_window")
 def test_rap_get_biomass(mock_open, test_3d_valid_xarray_epsg_4326):
     # Define the arguments for upload_fire_event
     rap_estimates = test_3d_valid_xarray_epsg_4326
@@ -31,7 +33,8 @@ def test_rap_get_biomass(mock_open, test_3d_valid_xarray_epsg_4326):
     # Convert the polygon to a GeoJSON
     square_geojson = gpd.GeoSeries([square_polygon]).__geo_interface__
 
-    mock_open.return_value = rap_estimates
+    mock_instance = mock_open.return_value
+    mock_instance.rio.isel_window.return_value = rap_estimates
     result = rap_get_biomass("2020-01-01", square_geojson, buffer_distance=1)
     # Add assertions to check the result
 
