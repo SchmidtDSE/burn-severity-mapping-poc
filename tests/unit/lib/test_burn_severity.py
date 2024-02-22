221bea16237885be1805474ed3c66e985c37e905
nick gondek <GondekNP@gmail.com>
2024-02-22 17:50:13 +0000

    pickle only the metadata and coords - that way, dynamically reconstruct xarrays with those coords that look just like whatever we've computed within pytests, using as fixtures

--- tests/unit/lib/test_burn_severity.py
+++ tests/unit/lib/test_burn_severity.py
@@ -3,25 +3,41 @@ import src.lib.burn_severity as burn_severity
 import xarray as xr
 
 
-def test_calc_nbr(test_4d_xarray, test_4d_zero_xarray, test_4d_nan_xarray):
+def test_calc_nbr(
+    test_4d_valid_xarray_epsg_4326,
+    test_4d_nan_xarray_epsg_4326,
+    test_4d_zero_xarray_epsg_4326,
+):
     """Test calc_nbr function."""
+
+    # Rename `band1` and `band2` to `B8A` and `B12` respectively
+    test_4d_valid_xarray_epsg_4326["band"] = [
+        "B8A",
+        "B12",
+    ]
+    test_4d_nan_xarray_epsg_4326["band"] = ["B8A", "B12"]
+    test_4d_zero_xarray_epsg_4326["band"] = [
+        "B8A",
+        "B12",
+    ]
+
     # Test valid input
-    xr_nir = test_4d_xarray.sel(band="B8A")
-    xr_swir = test_4d_xarray.sel(band="B12")
+    xr_nir = test_4d_valid_xarray_epsg_4326.sel(band="B8A")
+    xr_swir = test_4d_valid_xarray_epsg_4326.sel(band="B12")
     result = burn_severity.calc_nbr(xr_nir, xr_swir)
     assert result is not None
     assert result.shape == xr_swir.shape == xr_nir.shape
 
     # Test zero input - should work, but not meaningful
-    xr_nir = test_4d_zero_xarray.sel(band="B8A")
-    xr_swir = test_4d_zero_xarray.sel(band="B12")
+    xr_nir = test_4d_zero_xarray_epsg_4326.sel(band="B8A")
+    xr_swir = test_4d_zero_xarray_epsg_4326.sel(band="B12")
     result = burn_severity.calc_nbr(xr_nir, xr_swir)
     assert result is not None
     assert result.shape == xr_swir.shape == xr_nir.shape
 
     # Test NaN input - also should work, but not meaningful
-    xr_nir = test_4d_nan_xarray.sel(band="B8A")
-    xr_swir = test_4d_nan_xarray.sel(band="B12")
+    xr_nir = test_4d_nan_xarray_epsg_4326.sel(band="B8A")
+    xr_swir = test_4d_nan_xarray_epsg_4326.sel(band="B12")
     result = burn_severity.calc_nbr(xr_nir, xr_swir)
     assert result is not None
     assert result.shape == xr_swir.shape == xr_nir.shape
--- tests/unit/lib/test_query_rap.py
+++ tests/unit/lib/test_query_rap.py
@@ -2,14 +2,16 @@ import pytest
 from src.lib.query_rap import rap_get_biomass
 import unittest.mock as mock
 import xarray as xr
+import geopandas as gpd
+from shapely.geometry import Polygon
 
 
 @mock.patch("rioxarray.open_rasterio")
-def test_rap_get_biomass(mock_get, test_geojson, test_3d_xarray):
+def test_rap_get_biomass(mock_open, test_3d_valid_xarray_epsg_4326):
     # Define the arguments for upload_fire_event
-    rap_estimates = test_3d_xarray
+    rap_estimates = test_3d_valid_xarray_epsg_4326
 
-    # Duplicate the xarray to add two new bands
+    # Duplicate the xarray to add two new bands (just to have four, like RAP)
     rap_estimates = xr.concat([rap_estimates, rap_estimates], dim="band")
     rap_estimates["band"] = [
         "annual_forb_and_grass",
@@ -18,8 +20,24 @@ def test_rap_get_biomass(mock_get, test_geojson, test_3d_xarray):
         "tree",
     ]
 
-    mock_get.return_value.content = rap_estimates
-    result = rap_get_biomass("2020-01-01", test_geojson)
+    # Get the bounds
+    bounds = rap_estimates.rio.bounds()
+
+    # Create a square polygon using the bounds
+    square_polygon = Polygon(
+        [
+            (bounds[0], bounds[1]),
+            (bounds[0], bounds[3]),
+            (bounds[2], bounds[3]),
+            (bounds[2], bounds[1]),
+        ]
+    )
+
+    # Convert the polygon to a GeoJSON
+    square_geojson = gpd.GeoSeries([square_polygon]).__geo_interface__
+
+    mock_open.return_value = rap_estimates
+    result = rap_get_biomass("2020-01-01", square_geojson, buffer_distance=1)
     # Add assertions to check the result
 
     assert isinstance(result, xr.DataArray)
