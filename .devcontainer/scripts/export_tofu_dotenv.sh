9973e9a9059a618f92c41d8b881cf998dc638059
nick gondek <GondekNP@gmail.com>
2024-02-27 02:22:37 +0000

    fix path to container registry

--- .devcontainer/scripts/export_tofu_dotenv.sh
+++ .devcontainer/scripts/export_tofu_dotenv.sh
@@ -11,6 +11,7 @@ export gcp_service_account_s3_email=$(tofu output gcp_service_account_s3_email |
 
 echo "# TOFU ENV VARS" >> /workspace/.devcontainer/.env
 echo "ENV=LOCAL" >> /workspace/.devcontainer/.env
+echo "DEPLOYMENT=DEV" >> /workspace/.devcontainer/.env
 echo "S3_FROM_GCP_ROLE_ARN=$s3_from_gcp_role_arn" >> /workspace/.devcontainer/.env
 echo "GCP_CLOUD_RUN_ENDPOINT=$gcp_cloud_run_endpoint" >> /workspace/.devcontainer/.env
 echo "GCP_SERVICE_ACCOUNT_S3_EMAIL=$gcp_service_account_s3_email" >> /workspace/.devcontainer/.env
