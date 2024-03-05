0761a9be9479b52a2c534e444696c9bed7e2d3de
nick gondek <GondekNP@gmail.com>
2024-03-05 16:09:56 +0000

    fix bug in endpoint name for dev in tofu

--- .deployment/tofu/modules/burn_backend/main.tf
+++ .deployment/tofu/modules/burn_backend/main.tf
@@ -78,7 +78,7 @@ resource "google_cloud_run_v2_service" "tf-rest-burn-severity" {
       ## TODO [#24]: self-referential endpoint, will be solved by refactoring out titiler and/or making fully static
       env {
         name  = "GCP_CLOUD_RUN_ENDPOINT"
-        value = "${terraform.workspace}" == "prod" ? "https://tf-rest-burn-severity-ohi6r6qs2a-uc.a.run.app" : "https://tf-rest-burn-severity-dev-ohi6r6qs2a-uc.a.run.appz"
+        value = "${terraform.workspace}" == "prod" ? "https://tf-rest-burn-severity-ohi6r6qs2a-uc.a.run.app" : "https://tf-rest-burn-severity-dev-ohi6r6qs2a-uc.a.run.app"
       }
       env {
         name  = "CPL_VSIL_CURL_ALLOWED_EXTENSIONS"
