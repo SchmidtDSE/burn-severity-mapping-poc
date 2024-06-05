5403436cdb4ae5d22813d6441c9501e18d37d96f
GondekNP <npg@berkeley.edu>
2024-06-05 21:29:55 +0000

    a little security to check whether NaNs exist INSIDE or AT EDGE of spectral index values - since inside, if we replace w/ 0, we are likely to misidentify unburned islands!

--- src/lib/derive_boundary.py
+++ src/lib/derive_boundary.py
@@ -1,9 +1,10 @@
 import xarray as xr
 from scipy.ndimage import binary_fill_holes, gaussian_filter, binary_dilation
 from skimage.filters import threshold_otsu
-from skimage.segmentation import flood_fill
+from skimage.segmentation import flood_fill, clear_border
 from ..util.raster_to_poly import raster_mask_to_geojson
 from abc import ABC, abstractmethod
+import numpy as np
 
 
 ## THRESHOLDING STRATEGIES
@@ -14,9 +15,9 @@ class ThresholdingStrategy(ABC):
 
 
 class OtsuThreshold(ThresholdingStrategy):
-    def apply(self, metric_layer):
-        threshold = threshold_otsu(metric_layer)
-        burn_mask = metric_layer.where(metric_layer >= threshold, 1, 0)
+    def apply(self, metric_values):
+        threshold = threshold_otsu(metric_values)
+        burn_mask = metric_values.where(metric_values >= threshold, 1, 0)
         return burn_mask
 
 
@@ -24,8 +25,8 @@ class SimpleThreshold(ThresholdingStrategy):
     def __init__(self, threshold=0.5):
         self.threshold = threshold
 
-    def apply(self, metric_layer):
-        burn_mask = metric_layer.where(metric_layer >= self.threshold, 1, 0)
+    def apply(self, metric_values):
+        burn_mask = metric_values.where(metric_values >= self.threshold, 1, 0)
         return burn_mask
 
 
@@ -57,7 +58,24 @@ def derive_boundary(
     segmentation_strategy=FloodFillSegmentation(seed_location=(0, 0)),
 ):
 
-    burn_boundary_raster = thresholding_strategy.apply(metric_layer)
+    ## TODO: Some part of the spectral index process is creating a buffer of NaN
+    ## at the outside edge of the metric layer - not an issue to replace with 0 in this case
+    ## but zeros inside the image will be erroneously identified as unburned islands which is
+    ## a big problem.
+    metric_nan_mask = np.isnan(metric_layer.values)
+    metric_nan_cleared = clear_border(metric_layer.values, mask=metric_nan_mask)
+
+    if np.any(np.isnan(metric_nan_cleared)):
+        # In this case, we MAY be missing interior unburned islands
+        # if we proceed
+        raise ValueError("NaN values in metric layer")
+    else:
+        # In this case, we aren't missing interior unburned islands, but we still want the original
+        # shape preserved so we can use the mask to fill in the holes later and re-apply the spatial
+        # information to the final boundary
+        metric_values = np.nan_to_num(metric_layer.values, 0)
+
+    burn_boundary_raster = thresholding_strategy.apply(metric_values)
 
     burn_boundary_raster_postprocessed = postprocess_burn_mask(
         burn_boundary_raster, fill_holes=True, smooth=True, buffer=True
