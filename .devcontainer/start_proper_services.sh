c42bd45ef7d0a20675072b283a0bb1c81f4bb795
GondekNP <npg@berkeley.edu>
2024-06-22 20:11:41 +0000

    bungled logic - need to check for whether the developed service is this one, if so, tail to inifinity, if not start the relevant fastapi

--- .devcontainer/start_proper_services.sh
+++ .devcontainer/start_proper_services.sh
@@ -7,12 +7,12 @@ if [ "$DEVCONTAINER_SERVICE" = "$THIS_SERVICE" ]; then
     # Start a shell to keep container running
     tail -f /dev/null
 else
-    if [ "$DEVCONTAINER_SERVICE" != "titiler" ]; then
+    if [ "$THIS_SERVICE" = "titiler" ]; then
         # Start the frontend
         conda run -n titiler-dev uvicorn src.titiler.app:app --host=0.0.0.0 --port=8080
     fi
 
-    if [ "$DEVCONTAINER_SERVICE" != "burn_backend" ]; then
+    if [ "$THIS_SERVICE" = "burn_backend" ]; then
         # Start the REST API
         conda run -n burn-backend-dev uvicorn src.burn_backend.app:app --host=0.0.0.0 --port=5050
     fi
