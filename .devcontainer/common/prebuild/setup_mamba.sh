ed492d5513074833b1fc426ad9e8e24ed6a240a2
GondekNP <npg@berkeley.edu>
2024-07-15 14:20:42 -0700

    try making executible

--- .devcontainer/common/prebuild/setup_mamba.sh
+++ .devcontainer/common/prebuild/setup_mamba.sh
@@ -4,7 +4,13 @@
 set -e
 
 # Download and install Mambaforge version 24.3.0-0 (just a stable version)
-wget -qO- https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Mambaforge-Linux-x86_64.sh | bash -s -- -b -p /opt/mambaforge
+wget -qO- https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Mambaforge-Linux-x86_64.sh 
+
+# Make it executable
+chmod +x Mambaforge-Linux-x86_64.sh
+
+# Run the downloaded script
+bash ./Mambaforge-Linux-x86_64.sh -b -p /opt/mambaforge
 
 # Add Mambaforge to PATH
 echo 'export PATH=/opt/mambaforge/bin:$PATH' >>~/.profile
