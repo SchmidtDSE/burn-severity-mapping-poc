37d92f7dc9c6119cefe34cd6c8acda35cbae872f
GondekNP <npg@berkeley.edu>
2024-07-15 14:09:09 -0700

    omit init from the setup script

--- .devcontainer/common/prebuild/setup_mamba.sh
+++ .devcontainer/common/prebuild/setup_mamba.sh
@@ -6,9 +6,6 @@ set -e
 # Download and install Mambaforge version 24.3.0-0 (just a stable version)
 wget -qO- https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Mambaforge-Linux-x86_64.sh | bash -s -- -b -p /opt/mambaforge
 
-# Initialize shell
-/opt/mambaforge/bin/conda init
-
 # Add Mambaforge to PATH
 echo 'export PATH=/opt/mambaforge/bin:$PATH' >>~/.profile
 
