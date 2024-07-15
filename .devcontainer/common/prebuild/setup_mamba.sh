3af79b0e98f3dc96ae38be6d13605a5b8e9ed1e4
GondekNP <npg@berkeley.edu>
2024-07-15 14:22:58 -0700

    dont push to stdout

--- .devcontainer/common/prebuild/setup_mamba.sh
+++ .devcontainer/common/prebuild/setup_mamba.sh
@@ -4,13 +4,13 @@
 set -e
 
 # Download and install Mambaforge version 24.3.0-0 (just a stable version)
-wget -qO- https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Mambaforge-Linux-x86_64.sh 
+wget -qO- https://github.com/conda-forge/miniforge/releases/download/24.3.0-0/Mambaforge-Linux-x86_64.sh mambaforge_install.sh
 
 # Make it executable
-chmod +x Mambaforge-Linux-x86_64.sh
+chmod +x mambaforge_install.sh
 
 # Run the downloaded script
-bash ./Mambaforge-Linux-x86_64.sh -b -p /opt/mambaforge
+bash ./mambaforge_install.sh -b -p /opt/mambaforge
 
 # Add Mambaforge to PATH
 echo 'export PATH=/opt/mambaforge/bin:$PATH' >>~/.profile
